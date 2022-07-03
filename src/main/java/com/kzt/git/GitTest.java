eb31146be15a409e197c7500ddec50bc90ed117c
ZhiTao <ZhiTao@qq.com>
2022-07-03 15:39:06 +0800

    first commit...

---
+++ src/main/java/com/kzt/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.kzt.git;
+
+/**
+ * @Author ASUS
+ * Date 2022/7/3 15:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
